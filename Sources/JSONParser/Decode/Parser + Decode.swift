0ee476a0f630a8370eb54cbff869ed01e02cdd2a
Vaida <developer@vaidaf.net>
2025-11-02 22:56:37 +1100

    Amend

--- Sources/JSONParser/Decode/Parser + Decode.swift
+++ Sources/JSONParser/Decode/Parser + Decode.swift
@@ -26,7 +26,9 @@ extension JSONParser {
     /// Decodes a dictionary for the given key.
     public func decode(_ type: JSONParser.Type, forKey key: String) throws -> JSONParser {
         guard let data = self.contents[key] else { throw DecodeError.noSuchKey(key) }
-        return try JSONParser(data: data)
+        let parser = try JSONParser(data: data)
+        parser.keyDecodingStrategy = keyDecodingStrategy
+        return parser
     }
     
     /// Decodes a value of `String` for the given key.
